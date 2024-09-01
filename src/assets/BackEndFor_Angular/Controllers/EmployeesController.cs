29bae41cff7ae7d366eaed2f2abd9ce556be2afe
Bhavesh083 <cherrybobby6610@gmail.com>
2024-09-01 23:55:09 +0530

    Added API in assets folder

---
+++ src/assets/BackEndFor_Angular/Controllers/EmployeesController.cs
@@ -0,0 +1,73 @@
+﻿using BackEndFor_Angular.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Any;
+using System.Collections;
+using System.Numerics;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEndFor_Angular.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+        public static List<Employee> Employees_list = new List<Employee>()
+            {
+                new Employee(1,"Joseph",999,22,""),
+                new Employee(2,"Jhon",456,23,""),
+                new Employee(3,"Alex",788,34,""),
+                new Employee(4,"Stefan",1021,29,""),
+                new Employee(5,"Robert",1100,36,""),
+                new Employee(6,"Julia",990,37,""),
+                new Employee(7,"Rose",699,31,""),
+            };
+
+        public EmployeesController()
+        {
+        }
+
+        // GET: api/<EmployeesController>
+        [HttpGet]
+        public List<Employee> Get()
+        {
+            return Employees_list;
+        }
+
+        // GET api/<EmployeesController>/5
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            var employee = Employees_list.Find(element => element.employee_id == id);
+            return employee;
+        }
+
+        // POST api/<EmployeesController>
+        [HttpPost]
+        public Employee Post([FromBody] Employee emp)
+        {
+            Employees_list.Add(emp);
+            return emp;
+        }
+
+        // PUT api/<EmployeesController>/5
+        [HttpPut("{id}")]
+        public Employee Put(int id, [FromBody] Employee emp)
+        {
+            var employee = Employees_list.Find(element => element.employee_id == id);
+            employee = emp;
+            Employees_list.Add(employee);
+            return emp;
+        }
+
+        // DELETE api/<EmployeesController>/5
+        [HttpDelete("{id}")]
+        public int Delete(int id)
+        {
+            var index = Employees_list.FindIndex(element => element.employee_id == id);
+            Employees_list.RemoveAt(index);
+            return id;
+        }
+    }
+}
---
+++ src/assets/BackEndFor_Angular/Models/Employee.cs
@@ -0,0 +1,169 @@
+﻿namespace BackEndFor_Angular.Models
+{
+    public class Employee
+    {
+
+        public int employee_id { get; set; }
+        public string employee_name { get; set; }
+        public int employee_salary { get; set; }
+        public int employee_age { get; set; }
+        public string profile_image {  get; set; }
+
+        public Employee(int employee_id, string employee_name, int employee_salary, int employee_age, string profile_image)
+        {
+            this.employee_id = employee_id;
+            this.employee_name = employee_name;
+            this.employee_salary = employee_salary;
+            this.employee_age = employee_age;
+            this.profile_image = profile_image;
+        }
+    }
+}
+/*
+ "data": [
+        {
+            "id": 1,
+            "employee_name": "Tiger Nixon",
+            "employee_salary": 320800,
+            "employee_age": 61,
+            "profile_image": ""
+        },
+        {
+            "id": 2,
+            "employee_name": "Garrett Winters",
+            "employee_salary": 170750,
+            "employee_age": 63,
+            "profile_image": ""
+        },
+        {
+            "id": 3,
+            "employee_name": "Ashton Cox",
+            "employee_salary": 86000,
+            "employee_age": 66,
+            "profile_image": ""
+        },
+        {
+            "id": 4,
+            "employee_name": "Cedric Kelly",
+            "employee_salary": 433060,
+            "employee_age": 22,
+            "profile_image": ""
+        },
+        {
+            "id": 5,
+            "employee_name": "Airi Satou",
+            "employee_salary": 162700,
+            "employee_age": 33,
+            "profile_image": ""
+        },
+        {
+            "id": 6,
+            "employee_name": "Brielle Williamson",
+            "employee_salary": 372000,
+            "employee_age": 61,
+            "profile_image": ""
+        },
+        {
+            "id": 7,
+            "employee_name": "Herrod Chandler",
+            "employee_salary": 137500,
+            "employee_age": 59,
+            "profile_image": ""
+        },
+        {
+            "id": 8,
+            "employee_name": "Rhona Davidson",
+            "employee_salary": 327900,
+            "employee_age": 55,
+            "profile_image": ""
+        },
+        {
+            "id": 9,
+            "employee_name": "Colleen Hurst",
+            "employee_salary": 205500,
+            "employee_age": 39,
+            "profile_image": ""
+        },
+        {
+            "id": 10,
+            "employee_name": "Sonya Frost",
+            "employee_salary": 103600,
+            "employee_age": 23,
+            "profile_image": ""
+        },
+        {
+            "id": 11,
+            "employee_name": "Jena Gaines",
+            "employee_salary": 90560,
+            "employee_age": 30,
+            "profile_image": ""
+        },
+        {
+            "id": 12,
+            "employee_name": "Quinn Flynn",
+            "employee_salary": 342000,
+            "employee_age": 22,
+            "profile_image": ""
+        },
+        {
+            "id": 13,
+            "employee_name": "Charde Marshall",
+            "employee_salary": 470600,
+            "employee_age": 36,
+            "profile_image": ""
+        },
+        {
+            "id": 14,
+            "employee_name": "Haley Kennedy",
+            "employee_salary": 313500,
+            "employee_age": 43,
+            "profile_image": ""
+        },
+        {
+            "id": 15,
+            "employee_name": "Tatyana Fitzpatrick",
+            "employee_salary": 385750,
+            "employee_age": 19,
+            "profile_image": ""
+        },
+        {
+            "id": 16,
+            "employee_name": "Michael Silva",
+            "employee_salary": 198500,
+            "employee_age": 66,
+            "profile_image": ""
+        },
+        {
+            "id": 17,
+            "employee_name": "Paul Byrd",
+            "employee_salary": 725000,
+            "employee_age": 64,
+            "profile_image": ""
+        },
+        {
+            "id": 18,
+            "employee_name": "Gloria Little",
+            "employee_salary": 237500,
+            "employee_age": 59,
+            "profile_image": ""
+        },
+        {
+            "id": 19,
+            "employee_name": "Bradley Greer",
+            "employee_salary": 132000,
+            "employee_age": 41,
+            "profile_image": ""
+        },
+        {
+            "id": 20,
+            "employee_name": "Dai Rios",
+            "employee_salary": 217500,
+            "employee_age": 35,
+            "profile_image": ""
+        }, 
+ 
+ 
+ 
+ 
+ 
+ */
